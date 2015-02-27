098a3aea7c84810a9972bf630bc8044cfde38d00
Matija Kovačević <mata987@gmail.com>
2015-02-27 10:55:34 +0100

    Remove unnecessary post data from content loop

--- content.php
+++ content.php
@@ -9,19 +9,13 @@
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php demotheme_posted_on(); ?>
-		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
 			/* translators: %s: Name of current post */
-			the_content( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'demotheme' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+			the_post_thumbnail('large');
 		?>
 
 		<?php
@@ -32,7 +26,4 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php demotheme_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
